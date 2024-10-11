68ba915072bbc3214f1b700aa7af8dd845588fca
tolgahanbora <tolgahanbora@stu.aydin.edu.tr>
2024-10-11 04:29:32 +0300

    added new port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const rateLimit = require('express-rate-limit');
 const cacheManager = require('cache-manager');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 const app = express();
 
 app.use(express.json());
